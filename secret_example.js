22fe298907e527700d33b51f265d391f43c0ed72
lealhx2022 <95275565+lealhx2022@users.noreply.github.com>
2022-04-24 11:00:18 +0800

    Update secret_example.js

--- secret_example.js
+++ secret_example.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     MNEMONIC: "well egg .....",   // your 12 phrase words
     ACCOUNT_ADDR: "0x15D....",    // your account address
     INFURA_URL: "https://mainnet.infura.io/v3/<your infura key>",  // your infura free api key
-    OS_API_KEY: "<2941daee131e4a58932903083b94ee2c>",  // your opensea api key
+    OS_API_KEY: "<2>",  // your opensea api key
 });
